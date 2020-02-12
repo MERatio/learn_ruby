ef55634df10bd5b62c131120ce226f34b12d929a
Mark Edward Ratio <markedwardratio@gmail.com>
2020-02-12 13:28:52 +0800

    Complete exercises

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,26 @@
 class Timer
   #write your code here
+
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds
+    @seconds
+  end
+
+  def seconds=(seconds)
+    @seconds = seconds
+  end
+
+  def time_string
+    Time.at(@seconds).utc.strftime("%H:%M:%S") 
+  end
+
 end
+
+
+timer = Timer.new
+puts timer.seconds
+puts timer.seconds = 63
+puts timer.time_string
